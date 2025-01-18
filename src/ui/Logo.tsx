557b08b4ec03df9947a8e88be500ca909bd36e77
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-01-18 03:38:14 +0100

    feat: add the Logo ui component

---
+++ src/ui/Logo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledLogo = styled.div`
+	text-align: center;
+`;
+
+const Img = styled.img`
+	height: 9.6rem;
+	width: auto;
+`;
+
+const Logo = () => {
+	return (
+		<StyledLogo>
+			<Img src='/logo.svg' alt='logo' />
+		</StyledLogo>
+	);
+};
+
+export default Logo;

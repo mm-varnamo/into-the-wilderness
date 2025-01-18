f3aaed544eade77aa2ebd1bbf77ca87e04ce76d8
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-01-18 02:15:02 +0100

    feat: add the initial page components

---
+++ src/pages/Bookings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Row from '../ui/Row';
+import Heading from '../ui/Heading';
+
+const Bookings = () => {
+	return (
+		<Row type='horizontal'>
+			<Heading as='h1'>All bookings</Heading>
+			<p>Test</p>
+		</Row>
+	);
+};
+
+export default Bookings;

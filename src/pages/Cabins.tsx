3ca5677b9e46386b0e59c854fc51bdcd4c803c03
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-01-20 23:31:33 +0100

    feat: integrate CabinTable component into the Cabins page

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Row from '../ui/Row';
+import Heading from '../ui/Heading';
+import CabinTable from '../features/cabins/CabinTable';
 
 const Cabins = () => {
-	return <div>Cabins</div>;
+	return (
+		<>
+			<Row type='horizontal'>
+				<Heading as='h1'>All cabins</Heading>
+				<p>Filter / Sort</p>
+			</Row>
+
+			<Row type='vertical'>
+				<CabinTable />
+			</Row>
+		</>
+	);
 };
 
 export default Cabins;

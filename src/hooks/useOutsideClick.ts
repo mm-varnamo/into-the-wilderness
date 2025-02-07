4fad69594e65b1d313eaa2684728bf1a708999d0
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-02-07 20:51:36 +0100

    feat: close modal when clicking outside of it

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+const useOutsideClick = (handler: () => void, listenCapturing = true) => {
+	const ref = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleClick = (e: MouseEvent) => {
+			if (ref.current && !ref.current.contains(e.target as Node)) {
+				handler();
+			}
+		};
+
+		document.addEventListener('click', handleClick, listenCapturing);
+
+		return () =>
+			document.removeEventListener('click', handleClick, listenCapturing);
+	}, [handler, listenCapturing]);
+
+	return ref;
+};
+
+export default useOutsideClick;
--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -2,6 +2,7 @@ import { cloneElement, createContext, useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
+import useOutsideClick from '../hooks/useOutsideClick';
 
 const StyledModal = styled.div`
 	position: fixed;
@@ -105,11 +106,13 @@ const Window = ({ children, name }: WindowProps) => {
 
 	const { openName, close } = context;
 
+	const ref = useOutsideClick(close);
+
 	if (name !== openName) return null;
 
 	return createPortal(
 		<Overlay>
-			<StyledModal>
+			<StyledModal ref={ref}>
 				<Button onClick={close}>
 					<HiXMark />
 				</Button>
